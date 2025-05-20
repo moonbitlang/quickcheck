09d6985448b0d3494fe2e4d972f3756eb8a2da4b
CAIMEO <38813005+CAIMEOX@users.noreply.github.com>
2025-05-20 19:01:32 +0800

    chore: update to 0.9, drop x

--- rm_deps.js
+++ rm_deps.js
@@ -1,9 +1,4 @@
 const fs = require("fs");
 
-const raw = fs.readFileSync("moon.mod.json");
-const data = JSON.parse(raw);
-delete data["deps"];
-fs.writeFileSync("moon.mod.json", JSON.stringify(data, null, 2));
-
 fs.rmSync("src/internal_benchmark", { recursive: true });
 fs.rmSync("src/testing", { recursive: true });
